5b6e839ebb8731925f1ce28bec08d25212f44bac
Mateus B. Marques <mateusbiegermarques@gmail.com>
2023-03-07 15:05:38 -0300

    Primeiro commit

---
+++ MediNotas/app/Media_de_Notas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MediaDeNotas{
+    public static void Main(){
+        
+        double nota1, nota2, nota3;
+        
+        Console.WriteLine("Digite a 1 nota");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a 3 nota");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a 2 nota");
+        nota3 = Convert.ToDouble(Console.ReadLine());
+        
+        if((nota1+nota2+nota3) / 3 >= 7){
+            Console.WriteLine("voce passou");
+        }else{
+            Console.WriteLine("voce nao passou");
+        }
+        
+        }
+        
+}
+
+
---
+++ contagemRegressiva/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class ContagemRegressiva{
+    public static void Main(){
+        Console.WriteLine("Digite um numero para a contagem");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num < 0){
+            Console.WriteLine("O numero é negativo, o programa será encerrado");
+        }else{
+            while(num >= 0){
+                Console.WriteLine(num);
+                num--;
+            }
+        }
+    }
+}
